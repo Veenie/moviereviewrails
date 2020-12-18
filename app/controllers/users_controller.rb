93687fd03e676a6b439e30059a9f1562361c05da
Veenie <raveen08@gmail.com>
2020-12-18 15:36:00 -0800

    created sign up form, updated controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,21 @@ class UsersController < ApplicationController
 
     
     def new
+        @user = User.new
     end
 
     def create
+        @user =User.new(user_params)
+        if @user.save
+            #log in
+            session[:user_id] = @user.id
+            #redirect to show
+            redirect_to @user
+
+        else
+          render :new  
+        end  
+
     end
     
     def show
@@ -15,6 +27,12 @@ class UsersController < ApplicationController
 
     # def logout
     # end    
+
+    private
+
+    def user_params
+       params.require(:user).permit(:name, :password)
+    end    
 
 
 
