868d9ff8d1d28d54d8492fa5d017c9f566196745
Veenie <raveen08@gmail.com>
2020-12-19 17:14:18 -0800

    created log in form, create in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,18 @@
 class SessionsController < ApplicationController
     def home
     end
+
+    def create
+        user = User.find_by(name: params[:user][:name])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:message]
+            redirect_to '/login'
+        end
+    end
+
     
     def destroy
         session.clear
