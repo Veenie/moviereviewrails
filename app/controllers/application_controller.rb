855d5f008a92bec9ff6c928681433102f6ffffb5
Veenie <raveen08@gmail.com>
2020-12-19 17:49:53 -0800

    added helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    #if @current_user defined, use that, otherwise look for in table
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end 
+
+  def logged_in?
+    !!current_user
+  end
+  
+  def redirect_if_not_logged_in
+    redirect to '/' if !logged_in?
+  end 
+
+
+
 end
