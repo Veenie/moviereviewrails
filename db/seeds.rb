dae873d1a108cdedc2c1cf1979d8a2d0af9fe915
Veenie <raveen08@gmail.com>
2020-12-21 12:49:24 -0800

    added code for seeding movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create(title: 'The Dark Knight', year: 2008)
+Movie.create(title: 'The Dark Knight Rises', year: 2012)
+Movie.create(title: 'Batman Begins', year: 2005)
+Movie.create(title: 'Batman Returns', year: 1992)
+Movie.create(title: 'Batman', year: 1989)
+Movie.create(title: 'Batman Forever', year: 1995)
+Movie.create(title: 'Batman and Robin', year: 1997)
+Movie.create(title: 'The Batman', year: 2022)
