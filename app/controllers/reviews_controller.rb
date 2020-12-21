04e3fc147fa7c4602a6c0660254f6ba6784f9d4b
Veenie <raveen08@gmail.com>
2020-12-21 13:45:48 -0800

    filled out reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,6 +6,7 @@ class ReviewsController < ApplicationController
 
 
     def show
+      @review = Review.find_by(id: params[:id])
     end
 
     def new
@@ -13,17 +14,31 @@ class ReviewsController < ApplicationController
     end
 
     def create
+      @review = current.user.reviews.build(review_params)
+      if @review.save
+        redirect_to @review
+      else
+        render :new
+      end    
     end    
 
     def edit
+      @review = Review.find_by(id: params[:id])
     end
 
     def patch
+      @review = Review.find_by(id: params[:id])
+      @review.update(header: params[:review][:header], body: params[:review][:body])
+      redirect_to @review
     end    
 
-    def delete
-    end
+    # def delete
+    # end
 
+    private
 
+    def review_params
+       params.require(:review).permit(:header, :body)
+    end  
 
 end
