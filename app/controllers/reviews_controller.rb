8047914743d333b58dc03ccda658d8c6a2fa06c9
Veenie <raveen08@gmail.com>
2020-12-21 16:17:42 -0800

    added delete functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -23,26 +23,29 @@ class ReviewsController < ApplicationController
     end
 
     def create
-      @review = current.user.reviews.build(review_params)
-      if @review.save
-        redirect_to @review
+      review = current.user.reviews.build(review_params)
+      if review.save
+        redirect_to review_path(review)
       else
         render :new
       end    
-    end    
+    end
 
     def edit
       @review = Review.find_by(id: params[:id])
     end
 
     def update
-      @review = Review.find_by(id: params[:id])
-      @review.update(review_params)
-      redirect_to @review
-    end    
+      review = Review.find_by(id: params[:id])
+      review.update(review_params)
+      redirect_to review_path(review)
+    end
 
-    # def delete
-    # end
+    def delete
+      review = Review.find_by(id: params[:id])
+      review.destroy
+      redirect to '/reviews'
+    end
 
     private
 
