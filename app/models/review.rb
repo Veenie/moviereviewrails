8d0e2bba7ad2ffb3de26940c242a3935e3ae161b
Veenie <raveen08@gmail.com>
2020-12-21 19:28:53 -0800

    added a partial new and edit, adjusted the show page for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  validates :header, presence: true
+  validates :header, presence: true, uniqueness: true
   validates :body, presence: true
   belongs_to :user
   belongs_to :movie
