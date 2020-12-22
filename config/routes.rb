c7f9af39c970108c211c1cd36b6e0685b60843d9
Veenie <raveen08@gmail.com>
2020-12-21 21:02:22 -0800

    made my routes nested to fit requirements

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   end  
 
   resources :movies do
-    resources :reviews
+    resources :reviews, only: [:new, :create, :index]
   end
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
