789bfc216cc0b2a091b8733a55e35db808be3d2e
Veenie <raveen08@gmail.com>
2020-12-23 10:57:04 -0800

    improved linking and presentation on show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,6 @@ class UsersController < ApplicationController
         #redirect_if_not_logged_in
         #we use find_by rather than find because we get a nil result for our redirect
         @user = User.find_by_id(params[:id])
-        redirect_to '/' if !@user 
     end
 
     # def login
