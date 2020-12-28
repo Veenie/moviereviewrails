50c9fc7a9b3e8ad44630052eb321d3bd225bc904
Veenie <raveen08@gmail.com>
2020-12-28 15:15:57 -0800

    added some helpers to review controller, spaced the edit link

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,8 @@
 class ReviewsController < ApplicationController
+    before_action :set_review, only: [:show, :edit, :update]
+    before_action :review_auth, only: [:edit, :update]
 
     def show
-      @review = Review.find_by(id: params[:id])
-      redirect_to '/' if !@review
     end
 
     def new
@@ -20,13 +20,9 @@ class ReviewsController < ApplicationController
     end
 
     def edit
-      @review = Review.find_by(id: params[:id])
-      redirect_to review_path(@review) if !@review || @review.user != current_user
     end
 
     def update
-      @review = Review.find_by(id: params[:id])
-      redirect_to review_path(@review) if !@review || @review.user != current_user
       if @review.update(review_params)
         redirect_to review_path(@review)
       else
@@ -46,5 +42,20 @@ class ReviewsController < ApplicationController
     def review_params
        params.require(:review).permit(:header, :body, :movie_id)
     end
+
+    def set_review
+      @review = Review.find_by(id: params[:id])
+      if !@review
+        flash[:message] = "Review not found"
+        redirect_to '/'
+      end  
+    end
+
+    def review_auth
+      redirect_to review_path(@review) 
+      if @review.user != current_user
+        flash[:message] = "Unauthorized action"
+      end  
+    end  
 
 end
